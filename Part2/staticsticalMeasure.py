76018e5918e2dfac85fee7ea71051f6a4e63e9d1
manavtore <113475112+manavtore@users.noreply.github.com>
2023-12-07 07:53:58 +0530

    Update staticsticalMeasure.py

--- Part2/staticsticalMeasure.py
+++ Part2/staticsticalMeasure.py
@@ -1,56 +1,56 @@
-import numpy as np
-import matplotlib.pyplot as plt
+data = [4, 2, 7, 1, 8, 5, 2, 7, 2, 6]
 
-# Generate example data
-np.random.seed(42)
-data1 = np.random.normal(loc=5, scale=2, size=100)
-data2 = np.random.normal(loc=5, scale=2, size=100)
+# Mean
+mean = 0
+for num in data:
+    mean += num
+mean /= len(data)
+print(f"Mean: {mean}")
 
-# Compute statistical measures
-mean1 = np.mean(data1)
-mean2 = np.mean(data2)
+# Mode
+counts = {}
+for num in data:
+    if num in counts:
+        counts[num] += 1
+    else:
+        counts[num] = 1
+mode = None
+max_count = 0
+for num, count in counts.items():
+    if count > max_count:
+        mode = num
+        max_count = count
+print(f"Mode: {mode}")
 
-variance1 = np.var(data1)
-variance2 = np.var(data2)
+# Median
+sorted_data = sorted(data)
+n = len(sorted_data)
+if n % 2 == 0:
+    median = (sorted_data[n // 2 - 1] + sorted_data[n // 2]) / 2
+else:
+    median = sorted_data[n // 2]
+print(f"Median: {median}")
 
-std_dev1 = np.std(data1)
-std_dev2 = np.std(data2)
+# Standard Deviation
+variance = sum((x - mean)**2 for x in data) / (len(data) - 1)
+std_dev = variance**0.5
+print(f"Standard Deviation: {std_dev}")
 
-covariance = np.cov(data1, data2)[0, 1]
+# Covariance
+other_data = [3, 6, 8, 1, 9, 4, 5, 7, 2, 6]
+covariance = sum((data[i] - mean) * (other_data[i] - np.mean(other_data)) for i in range(len(data))) / (len(data) - 1)
+print(f"Covariance: {covariance}")
 
-correlation = np.corrcoef(data1, data2)[0, 1]
+# Correlation
+correlation = covariance / (std_dev * np.std(other_data))
+print(f"Correlation: {correlation}")
 
-standard_error1 = np.std(data1) / np.sqrt(len(data1))
-standard_error2 = np.std(data2) / np.sqrt(len(data2))
+# Standard Error
+se_mean = std_dev / (len(data)**0.5)
+print(f"Standard Error of the Mean: {se_mean}")
 
-# Display the distribution of samples graphically
-plt.figure(figsize=(12, 6))
-
-plt.subplot(1, 2, 1)
-plt.hist(data1, bins=20, color='blue', alpha=0.7, label='Data 1')
-plt.title('Distribution of Data 1')
-plt.xlabel('Value')
+plt.hist(data, bins=range(min(data), max(data) + 1), edgecolor='black', alpha=0.7)
+plt.title('Histogram of Example Data')
+plt.xlabel('Values')
 plt.ylabel('Frequency')
-plt.legend()
-
-plt.subplot(1, 2, 2)
-plt.hist(data2, bins=20, color='orange', alpha=0.7, label='Data 2')
-plt.title('Distribution of Data 2')
-plt.xlabel('Value')
-plt.ylabel('Frequency')
-plt.legend()
-
-plt.tight_layout()
 plt.show()
-
-# Print computed statistical measures
-print(f"Mean of Data 1: {mean1}")
-print(f"Mean of Data 2: {mean2}")
-print(f"Variance of Data 1: {variance1}")
-print(f"Variance of Data 2: {variance2}")
-print(f"Standard Deviation of Data 1: {std_dev1}")
-print(f"Standard Deviation of Data 2: {std_dev2}")
-print(f"Covariance between Data 1 and Data 2: {covariance}")
-print(f"Correlation between Data 1 and Data 2: {correlation}")
-print(f"Standard Error of Data 1: {standard_error1}")
-print(f"Standard Error of Data 2: {standard_error2}")
